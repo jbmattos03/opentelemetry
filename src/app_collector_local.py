4e331d07b498dd1f5dfa8c0ee3d78d61ad62c9c6
jbmattos03 <jbmattos2003@gmail.com>
2025-04-16 09:07:14 -0300

    fix: :bug: Fix endpoint for OTLPMetricExporter
    Fixed a bug that made it so some Python versions couldn't read the endpoint string until the end

--- src/app_collector_local.py
+++ src/app_collector_local.py
@@ -52,7 +52,7 @@ class SystemMonitor:
         This method sets up the exporters.
         The OTLP Collector is used to collect the data, then expose and send them to the Prometheus server.
         """
-        self.collector_exporter = OTLPMetricExporter(endpoint=f"http://{os.getenv("IP_ADDR")}:4318/v1/metrics")
+        self.collector_exporter = OTLPMetricExporter(endpoint=f"http://{os.getenv('IP_ADDR')}:4318/v1/metrics")
 
     def set_readers(self):
         """
