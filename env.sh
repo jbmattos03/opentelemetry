b0de501f7d15ee4cf909816fba14ab8507064bf6
jbmattos03 <jbmattos2003@gmail.com>
2025-04-17 17:49:57 -0300

    chore: :sparkles: Update env.sh
    Added a feature to create the .env file if it doesn't exist

--- env.sh
+++ env.sh
@@ -1,5 +1,13 @@
 IP_ADDRESS=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
 
+# Check if the .env file exists; if not, create it
+if [ ! -d ".env" ]; then
+    touch ./.env
+fi
+
+# Check if the IP_ADDR variable exists in the .env file
+# If it does, update it
+# If not, add it
 if grep -q "^IP_ADDR=" ./.env; then
     sed -i "s/^IP_ADDR=.*/IP_ADDR=$IP_ADDRESS/" ./.env
 else
