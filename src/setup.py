0441ba6d0a1be4d33b182e65803bbbb2e3562eb7
jbmattos03 <jbmattos2003@gmail.com>
2025-05-02 12:22:45 -0300

    build: :construction_worker: Update setup.py
    Removed app_collector_local.py and added name and compiler_directives attributes

--- src/setup.py
+++ src/setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("app_collector_local.py")
+    name="local_collector",
+    ext_modules=cythonize(["local_collector.pyx"]),
+    compiler_directives={"language_level": "3"},
 )
