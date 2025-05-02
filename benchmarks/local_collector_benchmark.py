a75ae2e7f3038f6958dd6abf8a0847b12834776d
jbmattos03 <jbmattos2003@gmail.com>
2025-05-02 12:23:43 -0300

    feat: :sparkles: Added a function to calculate the mean and std_deviation of n executions of the benchmarks

--- benchmarks/local_collector_benchmark.py
+++ benchmarks/local_collector_benchmark.py
@@ -17,6 +17,9 @@ def benchmark_system_monitor():
     ram = timeit.timeit(lambda: monitor.ram_callback(None), number=20000)
     if ram:
         print("RAM")
+    disk = timeit.timeit(lambda: monitor.disk_callback(None), number=20000)
+    if disk:
+        print("disk")
     disk_read = timeit.timeit(lambda: monitor.disk_read_callback(None), number=20000)
     if disk_read:
         print("disk_read")
@@ -32,10 +35,29 @@ def benchmark_system_monitor():
 
     return {"cpu": cpu, 
             "ram": ram, 
+            "disk": disk,
             "disk_read": disk_read, 
             "disk_write": disk_write, 
             "network_recv": network_recv, 
             "network_sent": network_sent}
+
+# Calculate mean and std dev for each metric
+def stats(n):
+    benchmark_results = []
+
+    for i in range(n):
+        print(f"Iteration {i}")
+        benchmark_results.append(benchmark_system_monitor())
+    
+    # Calculating the mean for each metric
+    mean = {}
+    std_dev = {} 
+    for metric in benchmark_results[0].keys():
+        mean[metric] = sum(result[metric] for result in benchmark_results) / len(benchmark_results)
+        std_dev[metric] = (sum((result[metric] - mean[metric]) ** 2 for result in benchmark_results) / len(benchmark_results)) ** 0.5
+    
+    return mean, std_dev
+
 
 # Run the benchmark
 if __name__ == "__main__":
@@ -43,3 +65,9 @@ if __name__ == "__main__":
     print("Benchmark Results:")
     for key, value in benchmark_results.items():
         print(f"{key}: {value:.6f} seconds")
+    
+
+    mean, std_dev = stats(10)
+    print("\nMean and Standard Deviation of Benchmark Results:")
+    for m, s in zip(mean.keys(), std_dev.keys()):
+        print(f"{m}: {mean[m]:.6f} \u00B1 {std_dev[s]:.6f} seconds")
